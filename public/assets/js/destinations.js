f175b41d73054501e2f5f488624858027200fa6e
kferrerux <ferrerkevin@hotmail.fr>
2023-12-07 09:38:50 +0100

    branch test

--- public/assets/js/destinations.js
+++ public/assets/js/destinations.js
@@ -8,7 +8,7 @@ let params = new URLSearchParams(window.location.search);
 // Conversion de la valeur de l'id en nombre entier
 let destination = parseInt(params.get("destination"));
 // Debug
-//console.log("Destination Param : ", destination);
+console.log("Destination Param : ", destination);
 
 // Appel de la fonction getDestination TODO: à compléter
 // getDestination(destinations, destination);
